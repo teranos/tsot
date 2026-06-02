074cd4ddbeab4bc925eabbdbbc9955b849a6a448
Brandon van Houten <brandon.vhouten@gmail.com>
2026-06-02 10:07:46 +0200

    fst mutation: red free-cast +2/+1 to host (Klotho-cycle protein name)

---
+++ cards/fst.lua
@@ -0,0 +1,18 @@
+-- Red mutation in the protein-gene cycle (Klotho/FST). Free to cast.
+-- Grants the host creature +2/+1 via an `attached_host`-scope static.
+-- Wired today (no missing-event dependency like klotho has) because
+-- the effect is a continuous stat modifier, not a triggered ability.
+return {
+  id = "fst",
+  name = "FST",
+  type = "mutation",
+  colors = {"red"},
+  cost = {},
+  abilities = {
+    "the host creature gets +2/+1.",
+  },
+  static = {
+    affects = {scope = "attached_host"},
+    modifier = {x = 2, y = 1},
+  },
+}
